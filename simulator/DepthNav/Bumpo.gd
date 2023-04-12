f7d7fe7aa71b9e1bc057b661e48bad2a21b8aeb1
Gabriel <gabrielrstella@tamu.edu>
2023-04-12 18:24:29 -0500

    working on cleaner nav

--- simulator/DepthNav/Bumpo.gd
+++ simulator/DepthNav/Bumpo.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 60.0
+const MOVE_SPEED = 60.0
 const TURN_SPEED = 8.0
 
 #ray scanning
@@ -10,15 +10,22 @@ const N_RAYS = 100
 const RAY_RANGE = 160
 const BACKUP_RANGE = 60 #if something is this close, the robot backs up
 
-var raycasts = [] #stores last measured distance
-var fraction_hit = 0
+var raycasts = [] #stores last measured distance, for drawing
+var vel = Vector2.ZERO
+var rot = 0
 
 #random modulation
 const N_RANDS = 2
 var rands = []
 var d_rands = [] #derivs
+var rand_min = 0.7
+var rand_max = 1.0
 
-var rotation_rate = 0.0
+#keep track of oscillations;
+#if it starts oscillating,
+#execute "turn" behavior for random amt of time
+var prev_rotation_sign = 0
+var rotation_oscillation = 0
 
 func _init():
 	#initialize raycasts array
@@ -27,7 +34,7 @@ func _init():
 	for i in range(N_RANDS):
 		rands.append(0.5)
 		d_rands.append(0)
-		
+
 func argmax(xs):
 	var arg = 0
 	var valmax = xs[0]
@@ -38,28 +45,24 @@ func argmax(xs):
 			valmax = val
 	return arg
 
-func get_modulation_a(i):
+func get_modulation(i):
+	return 1
 	var j = lerp(0, N_RANDS - 1, float(i) / (N_RAYS - 1))
 	return lerp(rands[floor(j)], rands[ceil(j)], fposmod(j, 1))
 	
-func get_modulation_b(i):
-	var part = float(i) / (N_RAYS - 1)
-	var part_halved = abs(part - 0.5) * 2
-	return max(part_halved * part_halved, 1 - fraction_hit)
-
 func _physics_process(delta):
 	var rng = RandomNumberGenerator.new()
 	#update random modulation
 	for i in range(N_RANDS):
 		d_rands[i] = (d_rands[i] + rng.randf_range(-0.1, 0.1)) * 0.99
-		rands[i] = clamp(rands[i] + d_rands[i] * delta, 0.8, 1.0)
+		rands[i] = clamp(rands[i] + d_rands[i] * delta, rand_min, rand_max)
 	#reset velocity to zero
-#	velocity = Vector2.ZERO
+	velocity = Vector2.ZERO
 	#raycasts
 	#also calculate overall movement direction
 	var movement_direction = Vector2.ZERO
-	var rays_hit = 0
 	for i in range(N_RAYS):
+		#cast ray
 		var angle = deg_to_rad(lerp(-FOV, FOV, float(i) / (N_RAYS - 1)))
 		var ray = Vector2(cos(angle), sin(angle)) * RAY_RANGE
 		var raycaster:RayCast2D = $RayCast
@@ -68,22 +71,26 @@ func _physics_process(delta):
 		var hit_dist = RAY_RANGE
 		if(raycaster.is_colliding()):
 			hit_dist = to_local(raycaster.get_collision_point()).length()
-			rays_hit += 1
 		raycasts[i] = hit_dist
 		#add to overall direction
 		#weighted by cos of angle and some randomness
-		var length = hit_dist * get_modulation_a(i) - BACKUP_RANGE
+		var length = hit_dist * get_modulation(i) - BACKUP_RANGE
 		if(length > 0):
 			length = length / (hit_dist - BACKUP_RANGE)
 		else:
 			length = length / BACKUP_RANGE
-		movement_direction += ray.normalized() * length * cos(angle) * get_modulation_b(i)
+		ray = ray.normalized() # * length * abs(sin(i))
+		#squish
+		ray.x *= length
+		ray.y *= sign(length)
+		if(length > 0):
+			ray.y *= pow(length, 2)
+		#
+		movement_direction += ray
 	#
-	fraction_hit = float(rays_hit) / N_RAYS
-#	movement_direction = lerp(movement_direction, -movement_direction, fraction_hit * fraction_hit)
-	#
+#	movement_direction = movement_direction / N_RAYS
 	movement_direction = movement_direction.normalized()
-	#modulate distances by randomness over time
+
 	#determine overall direction
 	#(go towards max dist)
 	
@@ -92,11 +99,26 @@ func _physics_process(delta):
 	#convert motor commands to velocity
 	
 	#move
-	var new_rotation_rate = atan2(movement_direction.y, movement_direction.x) * delta * TURN_SPEED
-	rotation_rate = lerp(rotation_rate, new_rotation_rate, 0.03) #smoothing
+	rot = atan2(movement_direction.y, movement_direction.x) * TURN_SPEED
+	var rotation_rate = rot * delta
 	rotate(rotation_rate)
-	var new_vel = Vector2(movement_direction.x * SPEED / (abs(movement_direction.y) + 1), 0).rotated(rotation)
-	velocity = lerp(velocity, new_vel, 0.03) #smoothing
+	#keep track of rotation oscillations; if they persist, enter "turn" mode
+	var rotation_sign = sign(rotation_rate)
+	var is_oscillation = rotation_sign != prev_rotation_sign
+	prev_rotation_sign = rotation_sign
+	if(is_oscillation):
+		rotation_oscillation += delta
+	else:
+		rotation_oscillation *= pow(0.1, delta) #decay by 90% every second
+	if(rotation_oscillation > 0.2):
+		print("aaaa")
+		rotate(rng.randf() * PI * 2)
+		rotation_oscillation = 0
+	print(rotation_oscillation)
+	
+	vel = Vector2(movement_direction.x * MOVE_SPEED, 0)
+	var new_vel = vel.rotated(rotation)
+	velocity = new_vel
 	move_and_slide()
 	
 func _process(_delta):
@@ -114,27 +136,30 @@ func _draw():
 			else:
 				color = Color.RED
 		draw_line(Vector2.ZERO, ray, color, -1.0, true)
+	#draw current random modulation
+	var draw_min = 20
+	var draw_max = 40
 	for i in range(N_RAYS - 1):
-		var rand1 = get_modulation_a(i) * 20 + 20
+		var rand1 = lerp(draw_min, draw_max, get_modulation(i))
 		var angle1 = deg_to_rad(lerp(-FOV, FOV, float(i) / (N_RAYS - 1)))
 		var vec1 = Vector2(cos(angle1), sin(angle1)) * rand1
-		var rand2 = get_modulation_a(i + 1) * 20 + 20
+		var rand2 = lerp(draw_min, draw_max, get_modulation(i + 1))
 		var angle2 = deg_to_rad(lerp(-FOV, FOV, float(i + 1) / (N_RAYS - 1)))
 		var vec2 = Vector2(cos(angle2), sin(angle2)) * rand2
 		draw_line(vec1, vec2, Color.PURPLE, -1.0, true)
-		
-		rand1 = get_modulation_b(i) * 20 + 20
-		angle1 = deg_to_rad(lerp(-FOV, FOV, float(i) / (N_RAYS - 1)))
-		vec1 = Vector2(cos(angle1), sin(angle1)) * rand1
-		rand2 = get_modulation_b(i + 1) * 20 + 20
-		angle2 = deg_to_rad(lerp(-FOV, FOV, float(i + 1) / (N_RAYS - 1)))
-		vec2 = Vector2(cos(angle2), sin(angle2)) * rand2
-		draw_line(vec1, vec2, Color.PURPLE, -1.0, true)
-		
-		
-		vec1 = vec1.normalized() * (20 + 20 * 0.8)
-		vec2 = vec2.normalized() * (20 + 20 * 0.8)
+	#
+	for i in range(N_RAYS - 1):
+		var d1 = lerp(draw_min, draw_max, rand_min)
+		var angle1 = deg_to_rad(lerp(-FOV, FOV, float(i) / (N_RAYS - 1)))
+		var vec1 = Vector2(cos(angle1), sin(angle1)) * d1
+		var d2 = lerp(draw_min, draw_max, rand_min)
+		var angle2 = deg_to_rad(lerp(-FOV, FOV, float(i + 1) / (N_RAYS - 1)))
+		var vec2 = Vector2(cos(angle2), sin(angle2)) * d2
+		#inner
 		draw_line(vec1, vec2, Color.BLACK, -1.0, true)
-		vec1 = vec1.normalized() * 40
-		vec2 = vec2.normalized() * 40
+		#outer
+		vec1 = vec1.normalized() * lerp(draw_min, draw_max, rand_max)
+		vec2 = vec2.normalized() * lerp(draw_min, draw_max, rand_max)
 		draw_line(vec1, vec2, Color.BLACK, -1.0, true)
+	#draw velocity
+	draw_line(Vector2.ZERO, vel.rotated(rot), Color.BLACK, -1.0, true)
