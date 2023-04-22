fc2054e26a3d571e9bb7072dd4d23898f9c36d46
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-21 23:13:04 -0500

    work on robot controller

--- robot/v1.py
+++ robot/v1.py
@@ -69,18 +69,23 @@ class SensorTimers:
         if name in self.timers:
             del self.timers[name]
 
-#microphone
+    def update(self, dt):
+        for timer in self.timers:
+            self.timers[timer] -= dt
+
+#depth detector
 #button press
 #face detector
-#depth detector
 #random noise modulator
+#microphone (for debug)
 
 ###############################################################################
 #motors and other effectors
 ###############################################################################
 
 #drive control
-#
+#button led
+#face expressions
 
 ###############################################################################
 #p-field stuff
@@ -90,34 +95,111 @@ class SensorTimers:
 #state machine management
 ###############################################################################
 
-###############################################################################
-#types of states
-###############################################################################
+class StateMachine:
+    def __init__(self):
+        self.states = [] #states are State objects
+        self.transitions = [] #for each State object, store a list of transitions; these are pairs of (predicate, destination state index)
+        self.current_state = 0
+
+    def add_state(self, state):
+        self.states.append(state)
+        self.transitions.append([])
+        return len(self.states) - 1 #index of this state
+
+    #add transition from src to dst when predicate is true
+    #predicate is a function that takes a robot controller and returns true/false
+    def add_transition(self, src_index, predicate, dst_index):
+        self.transitions[src_index].append((predicate, dst_index))
+
+    def update(self, robot, dt):
+        #update current state
+        self.states[self.current_state].update(robot, dt)
+        #test for transition
+        for t in self.transitions[self.current_state]:
+            pred = t[0]
+            dst = t[1]
+            if(pred(robot)):
+                #transition
+                self.states[self.current_state].cb_exit(robot)
+                self.current_state = dst
+                self.states[self.current_state].cb_enter(robot)
+                break
+
+class State:
+    def __init__(self, name, behaviors, on_enter=None, on_exit=None):
+        self.name = name
+        self.behaviors = behaviors
+        #called when the state is entered
+        #start timers, etc
+        self.on_enter = on_enter
+        #called when the state is exited
+        #for cleanup
+        self.on_exit = on_exit
+        #
+    
+    def cb_enter(self, robot:RoboController):
+        print("Entered state:", self.name)
+        if self.on_enter is not None:
+            self.on_enter(robot)
+    
+    def cb_exit(self, robot:RoboController):
+        if self.on_exit is not None:
+            self.on_exit(robot)
+    
+    #called every tick that this state is active
+    def update(self, robot:RoboController, dt):
+        self.behaviors.update(robot, dt)
 
 ###############################################################################
-#generic behavior superclass for IRM behaviors
+#generic behavior manager/superclass for IRM behaviors
 ###############################################################################
 
 #deals with behavior releasing/inhibiting
-class BehaviorManager(object):
-    pass
+class BehaviorManagerIRM(object):
+    def __init__(self):
+        pass
 
-class GenericBehavior(object):
-    def __init__(self, name, controller:RoboController):
+class GenericBehaviorIRM(object):
+    def __init__(self, name):
         self.name = name
-        self.controller = controller
         self.released = False
 
-    def releaser(self):
+    def releaser(self, robot:RoboController):
         return False
 
-    def callback_released(self):
+    def callback_released(self, robot:RoboController):
         pass
 
-    def callback_inhibited(self):
+    def callback_inhibited(self, robot:RoboController):
         pass
 
-    def update(self, dt):
+    def update(self, robot:RoboController, dt):
+        pass
+
+###############################################################################
+#generic behavior manager/superclass for sequential behaviors
+###############################################################################
+
+#deals with behavior ordering
+class BehaviorManagerSeq(object):
+    def __init__(self):
+        pass
+
+class GenericBehaviorSeq(object):
+    def __init__(self, name):
+        self.name = name
+        self.released = False
+
+    def releaser(self, robot:RoboController):
+        return False
+
+    def callback_released(self, robot:RoboController):
+        pass
+
+    def callback_inhibited(self, robot:RoboController):
+        pass
+
+    def update(self, robot:RoboController, dt):
         pass
 
 ###############################################################################
@@ -138,6 +220,16 @@ if __name__ == '__main__':
     #initial state
 
 
+# #when the robot is wandering around, looking for a face
+# class StateWander(State):
+# #when the robot has found a face and is getting their attention (bump)
+# class StateEngage(State):
+# #when the robot is running away
+# class StatePlay(State):
+# #when the robot has been tagged
+# class StateSleep(State):
+# #just in case
+# class StateDebug(State):
 
     #run loop
     r = rospy.Rate(60)
