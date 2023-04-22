968577afde05f98418fc293cafee53244412e53e
Jeff Hykin <jeff.hykin@gmail.com>
2023-04-22 17:07:25 -0500

    rename servers

--- desktop/face_server.js
+++ desktop/face_server.js
@@ -57,8 +57,6 @@ async function loadConfig() {
                 /:.+?\/\*UNQUE_ID_19827378957898240_port\*\//, `: ${JSON.stringify(config.desktop.web_socket_port)}/*UNQUE_ID_19827378957898240_port*/`
             )
     })
-
-    // run`python ${pathToProject}`
 }
 
 // load at the begining and every time something changes
